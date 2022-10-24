4de97b5522e0c0686719c0266c0e5a95b786ad0e
Arzoka <114647680+Arzoka@users.noreply.github.com>
2022-10-24 12:48:55 +0200

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,14 @@
+from traceback import format_exc
+from colorama import Fore
+
+#Color
+
+PlayerColor = Fore.CYAN
+CharacterColor = Fore.RED
+ChoiceMenuColor = Fore.GREEN
+TextColor = Fore.WHITE
+
+#Timing
+
+typetime = 0.06
+WaitTimeBetweenSentence = 0.3
