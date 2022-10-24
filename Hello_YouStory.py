ae51096f3bedb1bca461bd3eb12666a79b1a8694
Arzoka <114647680+Arzoka@users.noreply.github.com>
2022-10-24 11:56:30 +0200

    Update Hello_YouStory.py

--- Hello_YouStory.py
+++ Hello_YouStory.py
@@ -13,10 +13,12 @@ running = True
 active = True
 inventory = []
 typetime = 0.06
+WaitTimeBetweenSentence = 0.3
 PlayerColor = colorama.Fore.CYAN
 CharacterColor = colorama.Fore.RED
 ChoiceMenuColor = colorama.Fore.GREEN
 TextColor = colorama.Fore.WHITE
+Speaker = ""
 
 #Functions
 
@@ -24,36 +26,32 @@ def end():
     active = False
     quit()
 
-def wait(time):
-    time.sleep(time)
+def wait(GivenTime):
+    time.sleep(GivenTime)
 
-def sp(s,s2,v):
-        global typetime
-        global CharacterColor
+def speak(s,v):
+    global WaitTimeBetweenSentence
+    global Speaker
+    global typetime
+    global CharacterColor
 
-        sys.stdout.write(CharacterColor + s + " ")
+    sys.stdout.write(CharacterColor +Speaker + ": ")
 
-        for c in s2:
-            sys.stdout.write(TextColor + c)
-            sys.stdout.flush()
-            time.sleep(typetime)
+    wait(WaitTimeBetweenSentence)
 
-        if v == "+":
-            print('')
+    for c in s:
+        sys.stdout.write(TextColor + c)
+        sys.stdout.flush()
+        time.sleep(typetime)
+
+    if v == 1:
+        print('')
 
 while(active == True):
-    sp("???:","Hey!","+")
-    sp("???:","You there??","+")
-    sp("???:","Wake up!","+")
+    os.system("cls")
+    Speaker = "???"
+
+    speak("Hey!",1)
+    speak("You there!",1)
+    speak("Wake up!",1)
     end()
-
-    
-    
-    
-    
-    
-
-
-
-
-
